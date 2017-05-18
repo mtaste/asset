7c35e4ab05ebe95b4f75fababe268b6e8c905693
ken <feng36201@126.com>
2017-05-18 10:27:33 +0800

    Signed-off-by: ken <feng36201@126.com>

--- app/src/main/java/asset/com/asset/MainActivity.java
+++ app/src/main/java/asset/com/asset/MainActivity.java
@@ -1,13 +1,23 @@
 package asset.com.asset;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
-
+    private Button readUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readUser= (Button) findViewById(R.id.readUser);
+        readUser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this , UserListActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
